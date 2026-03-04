7b0b4e98eae0eb9bcf5fe3391886594c14068367
ryan-none <barcosryan2@gmail.com>
2026-03-04 20:30:54 +0800

    delete signin signup

--- finance-tracker/finance-tracker-backend/src/routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { signupController, signinController } from '../controllers/authController';
-
-const router = Router();
-
-router.post('/signup', signupController);
-router.post('/signin', signinController);
-
-export default router;
